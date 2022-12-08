96d0cd992cf91b270d99c2e664fa30dbfc64bf31
henryofjupiter <oshardhenry@gmail.com>
2022-12-08 17:57:08 -0500

    makes improvement to decision branching, also includes option to pick up items

--- ModuleSixMilestone.py
+++ ModuleSixMilestone.py
@@ -8,23 +8,20 @@ def instructions():
 
 # status update
 def status(currentlocat, inventory, itemsavail):
-    print('You are at:', currentlocat)
-    print('You see a', itemsavail)
-    print('Inventory:', inventory)
-
-
-# moving through locations
-# def room_mov(cur_location, user):
-#     """uses dictionary to change location"""
-#     new_loc = lands[cur_location][user]
-#     return new_loc
+    if itemsavail == 0:
+        print('You are at:', currentlocat)
+        print('Inventory:', inventory)
+    else:
+        print('You are at:', currentlocat)
+        print('You see a', itemsavail)
+        print('Inventory:', inventory)
 
 
 def input_loop(user):
     """nested in validate function to continuously
     ask for input from user"""
     while (user != 'go') or (user == directions):
-        users = input(">Enter Command\n").split()
+        users = input(">>ENTER COMMAND<<\n").split()
         return users
 
 
@@ -64,8 +61,8 @@ itemsGame = ['Spade', 'Heart', 'Diamond', 'Clubs']
 def main():
     lands = {
         'Grass Lands': {'Left': 'Swamp Lands'},
-        'Swamp Lands': {'down': 'Jungle Lands', 'right': 'Grass Lands', 'item': 'Spade'},
-        'Jungle Lands': {'up': 'Swamp Lands', 'left': 'The Abyss', 'right': ' Mountain Lands', 'down': 'Check Point',
+        'Swamp Lands': {'Down': 'Jungle Lands', 'Right': 'Grass Lands', 'item': 'Spade'},
+        'Jungle Lands': {'Up': 'Swamp Lands', 'Left': 'The Abyss', 'right': ' Mountain Lands', 'Down': 'Check Point',
                          'item': 'Heart'},
         'The Abyss': {'right': 'Jungle Lands'},
         'Mountain Lands': {'left': 'Jungle Lands', 'up': 'Desert Lands', 'item': 'Diamond'},
@@ -82,8 +79,9 @@ def main():
 
     # gameplay loop
     while True:
-        user_input = input('>Enter Command\n').split(' ')
+        user_input = input('>>ENTER COMMAND<<\n').split(' ')
         user_input = validate(user_input)
+        print('---------------------')
 
         if current_location == 'Grass Lands':
             if user_input == 'Left':
@@ -96,25 +94,51 @@ def main():
 
         # work on feature to pick up items
 
-        # elif current_location == 'Swamp Lands':
-        #     if (user_input == 'North') or (user_input == 'East'):
-        #         current_location = room_mov(current_location, user_input)
-        #         instruct(current_location)
-        #     elif (user_input in directions) and (user_input != 'North' or 'East'):
-        #         print('Wrong Turn')
-        #         instruct(current_location)
-        #     elif user_input not in directions:
-        #         print('Try again')
-        #
-        # elif current_location == 'Cellar':
-        #     if user_input not in directions:
-        #         print('Try again')
-        #     elif user_input in directions and user_input != 'West':
-        #         print('Wrong Turn')
-        #         instruct(current_location)
-        #     else:
-        #         current_location = room_mov(current_location, user_input)
-        #         instruct(current_location)
+        elif current_location == 'Swamp Lands':
+            if user_input == 'Down':
+                current_location = lands[current_location][user_input]
+                itemsavail = lands[current_location]['item']
+                status(current_location, inventory, itemsavail)
+                print('----------------------')
+            if user_input == 'Spade':
+                inventory.append(itemsavail)
+                status(current_location, inventory, itemsavail=0)
+                print('----------------------')
+
+            elif user_input == 'Right':
+                current_location = lands[current_location][user_input]
+                status(current_location, inventory, itemsavail=0)
+                print('----------------------')
+
+        elif current_location == 'Jungle Lands':
+            if user_input == 'Up' or user_input == 'Right':
+                current_location = lands[current_location][user_input]
+                itemsavail = lands[current_location]['item']
+                status(current_location, inventory, itemsavail)
+                print('----------------------')
+
+            if user_input == 'Heart':
+                inventory.append(itemsavail)
+                status(current_location, inventory, itemsavail=0)
+                print('----------------------')
+
+            elif user_input == 'Left':
+                status(current_location, inventory, itemsavail=0)
+                print('GAME OVER')
+                break
+            elif user_input == 'Down':
+                current_location = lands[current_location][user_input]
+                status(current_location, inventory, itemsavail=0)
+                if len(inventory) < 4:
+                    print('!WARNING! WARNING!')
+                    print('If you dont have all four')
+                    print('items collect them first')
+                    print('before proceeding')
+                    print('----------------------')
+                else:
+                    continue
+
+        # PICK UP ITEMS
 
 
 main()
